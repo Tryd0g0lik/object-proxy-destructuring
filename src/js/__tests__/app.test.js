4f074a03ce819e1f1e77881c37a2a3a34efa10ea
Tryd0g0lik <work80@mail.ru>
2023-04-08 13:12:11 +0700

    'eslint' applications it cleaning code was

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,29 @@
+import app from '../app';
+
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+    },
+  ],
+};
+
+test('test 01', () => {
+  expect(() => {
+    app(character);
+  }).not.toThrow();
+});
