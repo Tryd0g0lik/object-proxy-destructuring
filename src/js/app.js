56895bebf6329050faef3cd1080c9e6f6a056d04
Tryd0g0lik <work80@mail.ru>
2023-04-08 10:08:53 +0700

    distructuring function is completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,43 @@
-// TODO: write your code here
-import sum from './basic';
+const character = {
+	name: 'Лучник',
+	type: 'Bowman',
+	health: 50,
+	level: 3,
+	attack: 40,
+	defence: 10,
+	special: [
+		{
+			id: 8,
+			name: 'Двойной выстрел',
+			icon: 'http://...',
+			description: 'Двойной выстрел наносит двойной урон',
+		},
+		{
+			id: 9,
+			name: 'Нокаутирующий удар',
+			icon: 'http://...',
+			// <- обратите внимание, описание "засекречено"
+		},
+	],
+};
 
-console.log('worked');
+// const attack = {
+// 	id: 9,
+// 	name: 'Нокаутирующий удар',
+// 	icon: 'http://...',
+// };
 
-console.log(sum([1, 2]));
+function distructions(objs) {
+	const { ...datas } = objs;
+	const heroes = [];
+	for (const hero of datas.special) {
+		if (!hero === false) {
+			heroes.push(hero);
+		} else if (!hero) {
+			throw new Error(`Одно из св-в имеет значение "${hero}"`);
+		}
+	}
+	return heroes;
+}
+
+console.log(distructions(character));
